fe314473fa78f51b17d2026b04d581a00564ecd7
John Doe <johndoe@email.com>
2025-03-08 22:25:18 +0100

    Actual AAAAAA

--- back/api/models.py
+++ back/api/models.py
@@ -9,7 +9,7 @@ class Uploader(models.Model):
 
 class Post(models.Model):
     posterid = models.ForeignKey(Uploader, on_delete=models.CASCADE)
-    content = models.CharField(max_length=20)
+    content = models.CharField(max_length=50)
     def __str__(self):
         return f"posterid={self.posterid}, content={self.content}"
 
